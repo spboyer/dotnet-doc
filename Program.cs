4bd8c8d975899718e7adacd22b0e8b4b5223c7df
Shayne Boyer <spboyer@live.com>
2018-04-09 21:26:22 -0400

    feeling lucky and README

--- Program.cs
+++ Program.cs
@@ -14,30 +14,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConsoleApplication
 {
-  //launch - https://docs.microsoft.com/search/index?search=docker
-  //api - https://docs.microsoft.com/api/search?search=mvc&locale=en-us&$top=10
-
-  // {
-  // title: "Building your first ASP.NET Core MVC app with Visual Studio",
-  // url: "https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/",
-  // description: "Getting started with ASP.NET Core MVC and Visual Studio",
-  // lastUpdatedDate: "2016-10-14T00:00:00+00:00",
-  // iconType: "Article",
-  // breadcrumbs: [ ]
-  // },
-  // {
-  // title: "Microsoft.AspNetCore.Mvc.Infrastructure namespace",
-  // url: "https://docs.microsoft.com/en-us/aspnet/core/api/microsoft.aspnetcore.mvc.infrastructure",
-  // description: null,
-  // lastUpdatedDate: "2016-09-15T00:00:00+00:00",
-  // iconType: "Reference",
-  // breadcrumbs: [ ]
-  // },
-
   [HelpOption]
   [Command(ThrowOnUnexpectedArgument = false)]
   class Program
   {
+
+    [Option(Description = "Open first result", LongName = "lucky", ShortName = "l")]
+    public bool Lucky { get; set; } = true;
 
     [Option(Description = "Output option to console", LongName = "console", ShortName = "c")]
     public bool Inline { get; set; } = true;
@@ -46,26 +29,8 @@ namespace ConsoleApplication
     [Range(1, 25)]
     public int Results { get; set; } = 5;
 
-    private string _terms = string.Empty;
-    [Option(Description = "Search terms", ShortName = "t")]
-    public string Terms
-    {
-      get
-      {
-        if (String.IsNullOrEmpty(_terms))
-        {
-          return String.Join("+", RemainingArguments).Trim();
-        }
-        else
-        {
-          return _terms;
-        }
-      }
-      set
-      {
-        _terms = value;
-      }
-    }
+    [Argument(0)]
+    public string Terms { get; set; }
 
     public string[] RemainingArguments { get; }
 
@@ -74,70 +39,48 @@ namespace ConsoleApplication
 
     private void OnExecute(CommandLineApplication app)
     {
-      if (RemainingArguments.Length == 0 && String.IsNullOrEmpty(Terms))
+      if (String.IsNullOrEmpty(Terms))
       {
         app.ShowHelp();
       }
       else
       {
         string locale = CultureInfo.CurrentCulture.Name;
-        if (Inline)
+        if (Inline | Lucky)
         {
           string api = $"https://docs.microsoft.com/api/search?search={Terms}&locale={locale}&$top={Results}";
           Console.WriteLine(api);
-          //return;
 
           var items = GetResults(api);
 
-          Console.WriteLine("Items found: " + items.Result.count.ToString());
-          foreach (Result r in items.Result.results)
+          if (Lucky)
           {
-            Console.WriteLine(r.iconType);
-            Console.WriteLine(r.title);
-            Console.WriteLine(r.url);
-            Console.WriteLine(" ");
+            OpenResult(items.Result.results[0].url);
           }
 
+          if (Inline)
+          {
+            Console.WriteLine("Items found: " + items.Result.count.ToString());
+            foreach (Result r in items.Result.results)
+            {
+              Console.WriteLine(r.iconType);
+              Console.WriteLine(r.title);
+              Console.WriteLine(r.url);
+              Console.WriteLine(" ");
+            }
+          }
         }
         else
         {
-          var cmd = "open";
           string url = $"https://docs.microsoft.com/search/index?search={Terms}";
-
-          if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-          {
-            cmd = "xdg-open";
-          }
-
-          if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-          {
-            cmd = "open";
-          }
-
-          if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-          {
-            cmd = "start";
-          }
-
-          var psi = new ProcessStartInfo { FileName = cmd, Arguments = url };
-          var process = new Process { StartInfo = psi };
-          process.Start();
+          OpenResult(url);
         }
       }
     }
 
-/*
-    public static int Main_(string[] args)
+    private void OpenResult(string url)
     {
-
-      var csl = args.Any(a => a == "--console");
-
-      string locale = CultureInfo.CurrentCulture.Name;
-
-      string cmd = "open";
-      string terms = Parse(args);
-      string url = $"https://docs.microsoft.com/search/index?search={terms}";
-      string api = $"https://docs.microsoft.com/api/search?search={terms}&local={locale}&$top=50";
+      var cmd = "open";
 
       if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
       {
@@ -154,38 +97,10 @@ namespace ConsoleApplication
         cmd = "start";
       }
 
-      if (csl)
-      {
-        var items = GetResults(api);
-
-        Console.WriteLine("Items found: " + items.Result.count.ToString());
-        foreach (Result r in items.Result.results)
-        {
-          Console.WriteLine(r.iconType);
-          Console.WriteLine(r.title);
-          Console.WriteLine(r.url);
-          Console.WriteLine(" ");
-        }
-      }
-      else
-      {
-        var psi = new ProcessStartInfo
-        {
-          FileName = cmd,
-          Arguments = url
-        };
-
-        var process = new Process
-        {
-          StartInfo = psi,
-
-        };
-        process.Start();
-      }
-
-      return 0;
+      var psi = new ProcessStartInfo { FileName = cmd, Arguments = url };
+      var process = new Process { StartInfo = psi };
+      process.Start();
     }
-*/
 
     public async static Task<Docs> GetResults(string url)
     {
@@ -199,4 +114,5 @@ namespace ConsoleApplication
 
   }
 }
+
 
